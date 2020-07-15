a34b97813a2adcaa5c9b56f85585871580301684
AbaddonSoftware <donnie.l.baker@gmail.com>
2020-07-15 07:41:59 -0400

    reorganized the main example to make it easier to add examples

--- src/org/launchcode/techjobs_oo/Main.java
+++ src/org/launchcode/techjobs_oo/Main.java
@@ -1,9 +1,5 @@
 package org.launchcode.techjobs_oo;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
